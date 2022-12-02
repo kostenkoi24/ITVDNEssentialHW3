46bb644586124ad90cecaaee3ccf1905383d035c
kostenkoi24 <kostenko.i@gmail.com>
2022-12-02 12:57:50 +0200

    Add project files.

---
+++ Homework3_1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Homework3_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
